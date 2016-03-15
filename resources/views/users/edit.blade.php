c2cd2ec9d1792804be0962a0e0b9585d3eb969e8
quentinlarousse <quentinlazaroto@gmail.com>
2016-03-15 17:53:04 +0100

    form
    form

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,19 @@
+@extends('app')
+
+
+@section('content')
+
+    <div class="panel panel-default">
+        <div class="panel-heading">Mon compte</div>
+        <div class="panel-body">
+
+            {!! form::open($user) !!}}
+
+
+            {!! Form::close() !!}}
+
+
+
+
+        </div>
+    </div>
