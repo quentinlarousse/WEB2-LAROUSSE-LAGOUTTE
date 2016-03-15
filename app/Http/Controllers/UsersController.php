999bf0379294f79bfb61b246adc210922084b24d
quentinlarousse <quentinlazaroto@gmail.com>
2016-03-15 16:50:14 +0100

    profile1
    profile1

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Illuminate\Support\Facades\Auth;
+
+class UsersController extends Controller
+{
+
+//creation de notre page profile
+
+
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
+
+
+
+    public function getEdit(){
+
+        $user = Auth::user();
+        return view('user.edit', compact('user'));
+
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
+                    Blog
                 </a>
             </div>
 
